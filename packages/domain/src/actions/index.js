477770300fa8261e9b66adf7086f5046eddd1692
Carlos Azaustre <cazaustre@gmail.com>
2020-07-27 22:13:23 +0200

    fix: :fire: remove folders repeated

--- packages/domain/src/actions/index.js
+++
@@ -1,7 +0,0 @@
-const itemRepository = require('../../repositories').itemRepository;
-
-module.exports = {
-  createItem: require('./createItem')({ itemRepository }),
-  getAllItems: require('./getAllItems')({ itemRepository }),
-  getItem: require('./getItem')({ itemRepository }),
-};
